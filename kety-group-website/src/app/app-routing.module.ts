c732cc846556177db49f10a6202ab8df5da5ef8f
BereDarius <darius.bogdan3080@gmail.com>
2020-11-23 17:40:04 +0200

    added angular material

--- kety-group-website/src/app/app-routing.module.ts
+++ kety-group-website/src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
+import {HomePageComponent} from "./components/home-page/home-page.component";
+import {ServicesPageComponent} from "./components/services-page/services-page.component";
+import {ProjectsPageComponent} from "./components/projects-page/projects-page.component";
+import {AboutUsPageComponent} from "./components/about-us-page/about-us-page.component";
+import {ContactPageComponent} from "./components/contact-page/contact-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home-page', component: HomePageComponent },
+  { path: 'services-page', component: ServicesPageComponent },
+  { path: 'projects-page', component: ProjectsPageComponent },
+  { path: 'about-us-page', component: AboutUsPageComponent },
+  { path: 'contact-page', component: ContactPageComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    RouterModule,
+    CommonModule
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- kety-group-website/src/app/app.module.ts
+++ kety-group-website/src/app/app.module.ts
@@ -3,14 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatListModule } from "@angular/material/list";
+import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { AboutUsPageComponent } from './components/about-us-page/about-us-page.component';
+import { ServicesPageComponent } from './components/services-page/services-page.component';
+import { ProjectsPageComponent } from './components/projects-page/projects-page.component';
+import { ContactPageComponent } from './components/contact-page/contact-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    AboutUsPageComponent,
+    ServicesPageComponent,
+    ProjectsPageComponent,
+    ContactPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatListModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
